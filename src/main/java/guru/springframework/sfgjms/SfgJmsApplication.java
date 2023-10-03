cfa09e16c06a846052f414402ffae1870c97e78c
Massimo Barbero <mxmo.barbero@gmail.com>
2023-10-03 09:03:19 +0200

    local broker with docker configuration + update parent pom version

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,8 +1,8 @@
 package guru.springframework.sfgjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
+//import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+//import org.apache.activemq.artemis.core.server.ActiveMQServer;
+//import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,13 +13,13 @@ public class SfgJmsApplication {
 	//embedded server just for test development
 	//configuration come from ActiveMQ configuration
 	//Active MQ is an implementation of JMS, there are many other
-		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-				.setPersistenceEnabled(false)
-				.setJournalDirectory("target/data/journal")
-				.setSecurityEnabled(false)
-				.addAcceptorConfiguration("invm","vm://0"));
-
-		server.start();
+//		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//				.setPersistenceEnabled(false)
+//				.setJournalDirectory("target/data/journal")
+//				.setSecurityEnabled(false)
+//				.addAcceptorConfiguration("invm","vm://0"));
+//
+//		server.start();
 
 
 		SpringApplication.run(SfgJmsApplication.class, args);
